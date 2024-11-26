38297a51175520f236994ca24636a2a1d9a5cf70
Zarha <zarhaselene@gmail.com>
2024-11-26 10:24:24 +0100

    Bokstävers position i strängar

--- fmw24_prov-main/question2.js
+++ fmw24_prov-main/question2.js
@@ -10,6 +10,24 @@
 
 function equalLetterPositions(str1, str2) {
   // Din kod här
+  const longestString =
+    str1.length > str2.length ? str1.toLowerCase() : str2.toLowerCase();
+  const longestStringArray = longestString.split("");
+  const shortestString = str1.length > str2.length ? str2 : str1;
+
+  let equalityCounter = 0;
+
+  shortestString.split("").forEach((letter, index) => {
+    if (letter === longestStringArray[index]) {
+      equalityCounter++;
+    }
+  });
+
+  return equalityCounter;
 }
+
+console.log(equalLetterPositions("go", "DO")); // Returnerar 1 (o och O är på samma position i båda strängarna)
+console.log(equalLetterPositions("hello", "world")); // Returnerar 1 (l är på samma position i båda strängarna)
+console.log(equalLetterPositions("detsamma", "detta")); // Returnerar 4 (d, e, t, a är på samma position i båda strängarna)
 
 module.exports = equalLetterPositions;
