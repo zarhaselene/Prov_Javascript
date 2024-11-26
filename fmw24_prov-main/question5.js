87c7711324006ae6b6f703389bf330319a796e74
Zarha <zarhaselene@gmail.com>
2024-11-26 09:49:01 +0100

    5. Responsive Posts

--- fmw24_prov-main/question5.js
+++ fmw24_prov-main/question5.js
@@ -3,3 +3,30 @@
 // För varje post ska title och body placeras under varandra.
 // I desktop-vy ska posts visas i 3 kolumner.
 // I mobil-vy ska posts visas i en kolumn med 100% width.
+const postsContainer = document.getElementById("container");
+
+async function getPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  data.forEach((post) => {
+    // Create elements
+    const containerEl = document.createElement("div");
+    const headingEl = document.createElement("h2");
+    const postEl = document.createElement("p");
+
+    containerEl.className = "post";
+    headingEl.className = "heading";
+    postEl.className = "content";
+
+    // Add content to elements
+    headingEl.textContent = post.title;
+    postEl.textContent = post.body;
+
+    //Append elements
+    containerEl.appendChild(headingEl);
+    containerEl.appendChild(postEl);
+    postsContainer.appendChild(containerEl);
+  });
+}
+getPosts();
